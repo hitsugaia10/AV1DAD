bbe47c0e981eb7e5b0efa779242191d632ee7a29
Jonas Araujo <jonassilvadearaujo18@gmail.com>
2020-03-28 23:12:09 -0300

    Back-End da AV1

---
+++ backend/src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const OngController = require("./controllers/ongController");
+const incidentController = require("./controllers/incidentController");
+const profileController = require("./controllers/profileController");
+const sessionController = require("./controllers/sessionController");
+
+const routes = express.Router();
+
+routes.post('/sessions', sessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get("/profile", profileController.index);
+
+routes.post("/incidents",incidentController.create )
+routes.get("/incidents",incidentController.index )
+routes.delete("/incidents/:id",incidentController.delete)
+module.exports = routes;
